874ff8497a57e1359f75df76e506dd44c5b7d07c
Patrik Mana <patrik.mana07@gmail.com>
2025-03-11 21:29:56 +0100

    čištění

--- Assets/Scripts/IInteractable.cs
+++
@@ -1,6 +0,0 @@
-// using System.Collections.Generic;
-//
-// public interface IInteractable {
-//     // Vrátí seznam dostupných akcí pro tento objekt
-//     List<InteractionAction> GetInteractions();
-// }
