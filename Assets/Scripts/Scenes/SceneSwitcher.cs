0e3d7cd323d79b4fbf6f3b69c273bf8b21c79103
Pluhec <jakplu@seznam.cz>
2025-03-12 18:41:14 +0100

    Adding logic for the scene between the terminal

---
+++ Assets/Scripts/Scenes/SceneSwitcher.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public int sceneBuildIndex;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+        }
+    }
+}
