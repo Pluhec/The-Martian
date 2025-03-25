6f0e5094336f11abcc956ca474c32e9f30545778
Pluhec <jakplu@seznam.cz>
2025-03-25 22:23:18 +0100

    udajne by to melo fungovat, ale nefunguje to... a nevim proc

---
+++ Assets/Scripts/SolarPanelMinigame/Clean.cs
@@ -0,0 +1,70 @@
+using System;
+using UnityEngine;
+
+public class Clean : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+
+    [SerializeField] private Texture2D _dirtMaskBase;
+    [SerializeField] private Texture2D _brush;
+
+    [SerializeField] private Material _material;
+
+    private Texture2D _templateDirtMask;
+
+    private void Start()
+    {
+        CreateTexture();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (Physics.Raycast(_camera.ScreenPointToRay(Input.mousePosition), out RaycastHit hit))
+            {
+                Vector2 textureCoord = hit.textureCoord;
+
+                int pixelX = (int)(textureCoord.x * _templateDirtMask.width);
+                int pixelY = (int)(textureCoord.y * _templateDirtMask.height);
+
+                for (int x = 0; x < _brush.width; x++)
+                {
+                    for (int y = 0; y < _brush.height; y++)
+                    {
+                        Color pixelDirt = _brush.GetPixel(x, y);
+                        Color pixelDirtMask = _templateDirtMask.GetPixel(pixelX + x, pixelY + y);
+
+                        _templateDirtMask.SetPixel(pixelX + x,
+                            pixelY + y,
+                            new Color(0, pixelDirtMask.g * pixelDirt.g, 0));
+                    }
+                }
+
+                _templateDirtMask.Apply();
+            }
+        }
+    }
+
+    private void CreateTexture()
+    {
+        if (_dirtMaskBase == null)
+        {
+            Debug.LogError("Dirt Mask Base texture is not assigned!");
+            return;
+        }
+
+        _templateDirtMask = new Texture2D(_dirtMaskBase.width, _dirtMaskBase.height);
+        try
+        {
+            _templateDirtMask.SetPixels(_dirtMaskBase.GetPixels());
+            _templateDirtMask.Apply();
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError("Error while creating texture: " + ex.Message);
+        }
+
+        _material.SetTexture("_DirtMask", _templateDirtMask);
+    }
+}
