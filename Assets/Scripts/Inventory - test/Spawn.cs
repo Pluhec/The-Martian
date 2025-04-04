3adeb2c5f274f5de13c87a037dda9b26dc86f729
Patrik Mana <patrik.mana07@gmail.com>
2025-04-04 15:42:34 +0200

    zaklad inventare

---
+++ Assets/Scripts/Inventory - test/Spawn.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject item;
+    private Transform player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    public void SpawnDroppedItem()
+    {
+        Vector2 playerPos = new Vector2(player.position.x, player.position.y + 0.35f);
+        Instantiate(item, playerPos, Quaternion.identity);
+    }
+}
