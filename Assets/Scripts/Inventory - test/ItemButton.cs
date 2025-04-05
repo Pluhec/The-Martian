644b762a84807d040ce15489bbc9d4cc3ac88c7d
Patrik Mana <patrik.mana07@gmail.com>
2025-04-05 02:03:25 +0200

    nejakym zazrakem to jede - mezicommit

--- Assets/Scripts/Inventory - test/ItemButton.cs
+++ Assets/Scripts/Inventory - test/ItemButton.cs
@@ -1,14 +1,15 @@
-using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class ItemButton : MonoBehaviour, IPointerClickHandler
 {
-    Slot slot;
+    public int mainSlotIndex;
+    public int slotSize = 3;
+    private Inventory inventory;
 
     private void Awake()
     {
-        slot = GetComponentInParent<Slot>();
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
     }
 
     public void OnPointerClick(PointerEventData eventData)
@@ -20,7 +21,30 @@ public class ItemButton : MonoBehaviour, IPointerClickHandler
         else if (eventData.button == PointerEventData.InputButton.Right)
         {
             Debug.Log("Pravý klik!");
-            slot.DropItem();
+            DropItem();
+        }
+    }
+
+    private void DropItem()
+    {
+        for (int i = 0; i < slotSize; i++)
+        {
+            int index = mainSlotIndex + i;
+            Transform slot = inventory.slots[index].transform;
+
+            if (slot.childCount > 0)
+            {
+                GameObject child = slot.GetChild(0).gameObject;
+
+                // Spawn se dělá jen u hlavního itemu
+                if (i == 0 && child.TryGetComponent<Spawn>(out var spawn))
+                {
+                    spawn.SpawnDroppedItem();
+                }
+
+                Destroy(child);
+                inventory.isFull[index] = false;
+            }
         }
     }
 }
--- Assets/Scripts/RadialMenu/Item1.cs
+++ Assets/Scripts/RadialMenu/Item1.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Item1 : InteractableObject
 {
     private Inventory inventory;
-    public GameObject itemButton;
+    public GameObject itemButton; // hlavní prefab
+    public int slotSize = 1;      // velikost itemu ve slotech
 
     void Awake()
     {
@@ -17,23 +19,65 @@ public class Item1 : InteractableObject
     {
         if (action == "sebrat")
         {
-            for (int i = 0; i < inventory.slots.Length; i++)
+            for (int i = 0; i <= inventory.slots.Length - slotSize; i++)
             {
-                if (inventory.isFull[i] == false)
+                bool found = true;
+
+                for (int j = 0; j < slotSize; j++)
                 {
+                    if (inventory.isFull[i + j])
+                    {
+                        found = false;
+                        break;
+                    }
+                }
+
+                if (found)
+                {
+                    // Vložit hlavní item button
+                    GameObject mainItem = Instantiate(itemButton, inventory.slots[i].transform, false);
                     inventory.isFull[i] = true;
-                    Instantiate(itemButton, inventory.slots[i].transform, false);
+
+                    var itemScript = mainItem.GetComponent<ItemButton>();
+                    itemScript.mainSlotIndex = i;
+                    itemScript.slotSize = slotSize;
+
+                    // Generování placeholderů jako kopie hlavního, ale upravené
+                    for (int j = 1; j < slotSize; j++)
+                    {
+                        GameObject placeholder = Instantiate(itemButton, inventory.slots[i + j].transform, false);
+
+                        // Zneškodnění interakcí
+                        Destroy(placeholder.GetComponent<ItemButton>());
+                        Destroy(placeholder.GetComponent<Button>()); // pokud tam je
+
+                        // Průhlednost
+                        Image img = placeholder.GetComponent<Image>();
+                        if (img != null)
+                        {
+                            Color c = img.color;
+                            c.a = 0.35f;
+                            img.color = c;
+                        }
+
+                        // Označení jako placeholder
+                        var p = placeholder.AddComponent<ItemPlaceholder>();
+                        p.mainSlotIndex = i;
+
+                        inventory.isFull[i + j] = true;
+                    }
+
                     Destroy(gameObject);
                     break;
                 }
             }
         }
-        
+
         if (action == "nasednout")
         {
             Debug.Log("Nasednuto!");
         }
-        
+
         if (action == "opravit")
         {
             Debug.Log("Opraveno!!");
