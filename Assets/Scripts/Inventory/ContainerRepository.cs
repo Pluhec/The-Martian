540eb2f989819aa88676b66b5fd82c12da2200f8
Patrik Mana <patrik.mana07@gmail.com>
2025-05-25 13:04:20 +0200

    oprava bedny

--- Assets/Scripts/Inventory/ContainerRepository.cs
+++ Assets/Scripts/Inventory/ContainerRepository.cs
@@ -1,18 +1,47 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 /// Úschovna deaktivovaných beden v DontDestroyOnLoad
 public static class ContainerRepository
 {
     static readonly Dictionary<string, GameObject> map = new();
+    
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+    private static void Init()
+    {
+        // Zajistí, že se kód provede při startu a podporuje i restart v editoru
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
 
     public static void Register(string id, GameObject box)
     {
         if (!map.ContainsKey(id)) map.Add(id, box);
+        Debug.Log($"[ContainerRepository] Registrována bedna: {id}");
     }
 
     public static bool TryGet(string id, out GameObject box) =>
         map.TryGetValue(id, out box);
 
-    public static void Unregister(string id) => map.Remove(id);
+    public static void Unregister(string id) 
+    {
+        if (map.ContainsKey(id))
+        {
+            map.Remove(id);
+            Debug.Log($"[ContainerRepository] Odregistrována bedna: {id}");
+        }
+    }
+
+    private static void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // Zajistí, že všechny bedny v repozitáři zůstanou neaktivní po načtení scény
+        foreach (var container in map.Values)
+        {
+            if (container != null && container.activeSelf)
+            {
+                container.SetActive(false);
+                Debug.Log($"[ContainerRepository] Deaktivována bedna při načtení scény: {scene.name}");
+            }
+        }
+    }
 }
